90b64c48ce8b15f1cb3826e8fbf9c031918009bd
John Huddleston <jlhudd@uw.edu>
2015-09-01 11:40:23 -0700

    added logic to the config shell script to check for modules environment.

--- config.sh
+++ config.sh
@@ -1,15 +1,21 @@
 export SMRT_ROOT=/net/eichler/vol24/projects/sequencing/pacbio/software/smrtanalysis
 
-module load anaconda/2.1.0
-module load bedtools/2.23.0
-module load blasr/dev
-module load freebayes/0.9.14
-module load samtools/1.1
+# Use modules to load dependencies if that environment is available.
+if [[ ! -z `which module` ]]
+then
+    module load bedtools/2.23.0
+    module load blasr/dev
+    module load freebayes/0.9.14
+    module load samtools/1.1
 
-module load hdf5/1.8.13
-module load netcdf/4.3.2
-module load R/3.1.0
-module load perl/5.14.2
-module load RepeatMasker/3.3.0
+    module load hdf5/1.8.13
+    module load netcdf/4.3.2
+    module load R/3.1.0
+    module load perl/5.14.2
+    module load RepeatMasker/3.3.0
 
-module load mpc/0.8.2 gmp/5.0.2 gcc/4.9.1
+    module load mpc/0.8.2 gmp/5.0.2 gcc/4.9.1
+else
+    export ANACONDA_ROOT=$HOME/anaconda
+    export PATH=$ANACONDA_ROOT/envs/python2/bin:$ANACONDA_ROOT/envs/python3/bin:$PATH
+fi
