493d27f34202dc10058bd513a4b76c190a04549b
John Huddleston <jlhudd@uw.edu>
2015-10-19 10:29:48 -0700

    added main wrapper for calling SMRT SV.

---
+++ smrtsv.py
@@ -0,0 +1,44 @@
+import argparse
+import subprocess
+import sys
+
+
+def index(args):
+    args = ("snakemake", "-q", "prepare_reference", "--config", "reference=%s" % args.reference)
+    print(args)
+    return subprocess.call(args)
+
+def align(args):
+    print "Align reads"
+
+def call(args):
+    print "Call variants"
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+
+    # Index a reference for use by BLASR.
+    parser_index = subparsers.add_parser("index", help="index a reference sequence for use by BLASR")
+    parser_index.add_argument("reference", help="FASTA file of reference to index")
+    parser_index.set_defaults(func=index)
+
+    # Align PacBio reads to an indexed reference with BLASR.
+    parser_align = subparsers.add_parser("align", help="align PacBio reads to an indexed reference with BLASR")
+    parser_align.add_argument("reference", help="FASTA file of indexed reference with .ctab and .sa in the same directory")
+    parser_align.add_argument("reads", help="text file with one absolute path to a PacBio reads file (.bax.h5) per line")
+    parser_align.add_argument("--alignments", help="text file with one absolute path to a BLASR alignments file (.bam) per line", default="alignments.fofn")
+    parser_align.add_argument("--alignments_dir", help="absolute path of directory for BLASR alignment files", default="alignments")
+    parser_align.set_defaults(func=align)
+
+    # Call SVs and indels from BLASR alignments.
+    parser_caller = subparsers.add_parser("call", help="call SVs and indels by local assembly of BLASR-aligned reads")
+    parser_caller.add_argument("reference", help="FASTA file of indexed reference with .ctab and .sa in the same directory")
+    parser_caller.add_argument("alignments", help="text file with one absolute path to a BLASR alignments file (.bam) per line")
+    parser_caller.add_argument("variants", help="VCF of variants called by local assembly alignments")
+    parser_caller.set_defaults(func=call)
+
+    args = parser.parse_args()
+    return_code = args.func(args)
+    sys.exit(return_code)
