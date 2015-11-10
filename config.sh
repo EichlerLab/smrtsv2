4d6910b113b5e5434481c891e56144492c44d1a5
John Huddleston <jlhudd@uw.edu>
2015-11-10 14:58:06 -0800

    updated config to use as few modules as possible for now.

--- config.sh
+++ config.sh
@@ -1,23 +1,24 @@
+export INSTALL_DIR=`pwd`
 export SMRT_ROOT=/net/eichler/vol24/projects/sequencing/pacbio/software/smrtanalysis
 export PATH=`pwd`/bin:$PATH
+export PATH=${INSTALL_DIR}/dist/miniconda/envs/python2/bin:${INSTALL_DIR}/dist/miniconda/envs/python3/bin:${INSTALL_DIR}/dist/miniconda/bin:$PATH
 
 # Use modules to load dependencies if that environment is available.
 module list &> /dev/null
 
 if [[ "$?" -eq "0" ]]
 then
-    module load anaconda/2.3.0
-    module load samtools/1.2-complete
+    module purge
+    module load modules modules-init modules-gs/prod modules-eichler/prod
+    #module load anaconda/2.3.0
+    #module load samtools/1.2-complete
     module load bedops/2.4.0
 
-    module load hdf5/1.8.13
-    module load netcdf/4.3.2
-    module load R/3.1.0
+    #module load hdf5/1.8.13
+    #module load netcdf/4.3.2
+    module load R/2.15.0
     module load perl/5.14.2
     module load RepeatMasker/3.3.0
 
-    module load mpfr/3.1.0 mpc/0.8.2 gmp/5.0.2 gcc/4.9.1
-else
-    export ANACONDA_ROOT=$HOME/anaconda
-    export PATH=$ANACONDA_ROOT/envs/python2/bin:$ANACONDA_ROOT/envs/python3/bin:$PATH
+    #module load mpfr/3.1.0 mpc/0.8.2 gmp/5.0.2 gcc/4.9.1
 fi
