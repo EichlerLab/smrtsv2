f9276f72e221b1492b54bbe778392f3b39241774
John Huddleston <jlhudd@uw.edu>
2016-04-11 11:13:51 -0700

    removed references to RedHat modules.

--- config.sh
+++ config.sh
@@ -2,25 +2,5 @@ export PATH=/usr/local/bin:/usr/bin:/bin
 export INSTALL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${INSTALL_DIR}/dist/hdf5/lib
 
-# Use modules to load dependencies if that environment is available.
-module list &> /dev/null
-
-if [[ "$?" -eq "0" ]]
-then
-    module purge
-    module load modules modules-init modules-gs/prod modules-eichler/prod
-    #module load anaconda/2.3.0
-    #module load samtools/1.2-complete
-    module load bedops/2.4.0
-
-    #module load hdf5/1.8.13
-    #module load netcdf/4.3.2
-    module load R/2.15.0
-    module load perl/5.14.2
-    module load RepeatMasker/3.3.0
-    module load java/8u25
-    module load mpfr/3.1.0 mpc/0.8.2 gmp/5.0.2 gcc/4.9.1
-fi
-
 export PATH=${INSTALL_DIR}/bin:$PATH
 export PATH=${INSTALL_DIR}/dist/miniconda/envs/python2/bin:${INSTALL_DIR}/dist/miniconda/envs/python3/bin:${INSTALL_DIR}/dist/miniconda/bin:${INSTALL_DIR}/dist/celera/wgs-8.3rc2/Linux-amd64/bin/:${INSTALL_DIR}/dist/amos-3.1.0/bin:${INSTALL_DIR}/canu/Linux-amd64/bin:$PATH
