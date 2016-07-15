4b112467b37da01a14a489b388af418fbbb4786a
John Huddleston <jlhudd@uw.edu>
2016-07-15 14:41:42 -0700

    refactored code to get best alignments for given regions into a reusable function, replaced subprocess call to get best alignments with a python function call in the genotyper, and replaced call to samtools depth with a python function that calculates depth across a list of alignments.

--- scripts/genotype.py
+++ scripts/genotype.py
@@ -3,13 +3,12 @@ Genotype a set of SV calls in a given set of BAMs.
 """
 import argparse
 from collections import defaultdict
-from joblib import Parallel, delayed
 import logging
 import numpy as np
 import pybedtools
 import pysam
-import subprocess
-import tempfile
+
+from get_best_alignment import get_best_alignments, get_depth_by_reference_and_position
 
 np.random.seed(1)
 
@@ -43,7 +42,7 @@ CONTIG_START=9
 CONTIG_END=10
 
 
-def get_depth_for_regions(bam_fields, alt_breakpoints, ref_breakpoints):
+def get_depth_for_regions(bam_fields, alt_breakpoints, ref_breakpoints, min_mapping_quality, min_base_quality):
     """
     Return median minus standard deviation of read depth across variant
     breakpoints in the given alternate and reference haplotypes.
@@ -56,34 +55,13 @@ def get_depth_for_regions(bam_fields, alt_breakpoints, ref_breakpoints):
     logger.debug("Min depth for alt region %s (size: %s)", alt_region, alt_region_size)
     logger.debug("Min depth for ref region %s (size: %s)", ref_region, ref_region_size)
 
-    arguments = {
-        "BAM": bam_fields["filename"],
-        "REF_REGION": ref_region,
-        "ALT_REGION": alt_region,
-        "TMPDIR": tempfile.gettempdir()
-    }
-    command = "samtools view -q 20 -bu %(BAM)s '%(REF_REGION)s' '%(ALT_REGION)s' | samtools sort -l 0 -n -O bam -T %(TMPDIR)s/reads - | python ~jlhudd/src/smrtsv/scripts/get_best_alignment.py /dev/stdin | samtools sort -l 0 -O bam -T %(TMPDIR)s/filtered_reads - | samtools depth -q 20 -Q 20 -" % arguments
-    logger.debug("Running command: %s", command)
-    result = subprocess.check_output(command, shell=True)
-    alt_depths = []
-    ref_depths = []
-    for line in result.splitlines():
-        contig, position, depth = line.split("\t")
-        position = int(position)
-        depth = int(depth)
+    best_alignments = get_best_alignments(bam_fields["file"], (ref_region, alt_region), quality=min_mapping_quality)
+    depth_by_reference_and_position = get_depth_by_reference_and_position(best_alignments, bam_fields["file"], base_quality=min_base_quality)
 
-        if contig == ref_breakpoints[0] and position >= ref_breakpoints[1] and position <= ref_breakpoints[2]:
-            ref_depths.append(depth)
-        elif contig == alt_breakpoints[0] and position >= alt_breakpoints[1] and position <= alt_breakpoints[2]:
-            alt_depths.append(depth)
-
-    # If there are not depths for every base of the given region, fill those
-    # missing values with enough additional zeroes to equal the region size.
-    if len(alt_depths) < alt_region_size:
-        alt_depths = alt_depths + [0] * (alt_region_size - len(alt_depths))
-
-    if len(ref_depths) < ref_region_size:
-        ref_depths = ref_depths + [0] * (ref_region_size - len(ref_depths))
+    alt_depths = [depth_by_reference_and_position.get(alt_breakpoints[0], {}).get(i, 0)
+                  for i in xrange(alt_breakpoints[1], alt_breakpoints[2] + 1)]
+    ref_depths = [depth_by_reference_and_position.get(ref_breakpoints[0], {}).get(i, 0)
+                  for i in xrange(ref_breakpoints[1], ref_breakpoints[2] + 1)]
 
     logger.debug("Found %i depths for region %s: %s", len(alt_depths), alt_region, alt_depths)
     logger.debug("Found %i depths for region %s: %s", len(ref_depths), ref_region, ref_depths)
@@ -97,22 +75,20 @@ def get_depth_for_regions(bam_fields, alt_breakpoints, ref_breakpoints):
     return high_quality_alt_depth, high_quality_ref_depth
 
 
-def get_depth_for_sv_call(sv_call, bams_by_name, chromosome_sizes):
-    SLOP_FOR_BREAKPOINTS = 25
-
+def get_depth_for_sv_call(sv_call, bams_by_name, chromosome_sizes, min_mapping_quality, min_base_quality, slop_for_insertion_breakpoints):
     if sv_call[EVENT_TYPE] == "deletion":
-        breakpoint_intervals = (sv_call[CONTIG_NAME], max(0, int(sv_call[CONTIG_START]) - SLOP_FOR_BREAKPOINTS), int(sv_call[CONTIG_END]) + SLOP_FOR_BREAKPOINTS)
+        breakpoint_intervals = (sv_call[CONTIG_NAME], max(0, int(sv_call[CONTIG_START]) - slop_for_insertion_breakpoints), int(sv_call[CONTIG_END]) + slop_for_insertion_breakpoints)
         reference_intervals = (sv_call[CHROMOSOME], int(sv_call[START]), int(sv_call[END]))
         reference_call_type = "insertion"
     else:
         breakpoint_intervals = (sv_call[CONTIG_NAME], int(sv_call[CONTIG_START]), int(sv_call[CONTIG_END]))
-        reference_intervals = (sv_call[CHROMOSOME], max(0, int(sv_call[START]) - SLOP_FOR_BREAKPOINTS), int(sv_call[START]) + SLOP_FOR_BREAKPOINTS)
+        reference_intervals = (sv_call[CHROMOSOME], max(0, int(sv_call[START]) - slop_for_insertion_breakpoints), int(sv_call[START]) + slop_for_insertion_breakpoints)
         reference_call_type = "deletion"
 
     logger.debug("Breakpoint intervals: %s", breakpoint_intervals)
 
     for bam_name, bam in bams_by_name.iteritems():
-        breakpoint_concordant_depth, breakpoint_discordant_depth = get_depth_for_regions(bam, breakpoint_intervals, reference_intervals)
+        breakpoint_concordant_depth, breakpoint_discordant_depth = get_depth_for_regions(bam, breakpoint_intervals, reference_intervals, min_mapping_quality, min_base_quality)
         logger.debug("Found concordant/discordant depth for %s: %s / %s", sv_call[EVENT_TYPE], breakpoint_concordant_depth, breakpoint_discordant_depth)
 
         print("\t".join(map(str, (
@@ -134,7 +110,9 @@ if __name__ == "__main__":
     parser.add_argument("sv_calls", help="BED file of SV calls with reference coordinates in columns 1-3, SV type in 4, length in 5, and contig coordinates in 6-8")
     parser.add_argument("bams", nargs="+", help="one or more BAMs per sample to genotype; read groups must be present in BAM")
     parser.add_argument("--debug", action="store_true")
-    parser.add_argument("--nproc", type=int, default=1, help="number of processes to run in parallel")
+    parser.add_argument("--min_mapping_quality", type=int, default=20, help="minimum mapping quality of alignments to consider for genotyping")
+    parser.add_argument("--min_base_quality", type=int, default=20, help="minimum base quality to consider for calculating read depth per haplotype")
+    parser.add_argument("--slop_for_insertion_breakpoints", type=int, default=25, help="number of bases on either side of an insertion breakpoint to calculate read depth across")
     args = parser.parse_args()
 
     if args.debug:
@@ -154,16 +132,13 @@ if __name__ == "__main__":
             chromosome_sizes = dict(zip(bam_file.references, [(0, length) for length in bam_file.lengths]))
             logger.debug("Got %i chromosome sizes", len(chromosome_sizes))
 
-        bams_by_name[bam]["file"] = None
+        bams_by_name[bam]["file"] = bam_file
         bams_by_name[bam]["filename"] = bam
         bams_by_name[bam]["sample"] = bam_file.header["RG"][0]["SM"]
-        bam_file.close()
 
     columns = ("sample", "chr", "start", "end", "sv_call", "contig", "contig_start", "contig_end", "concordant", "discordant")
     print("\t".join(columns))
 
     sv_calls = pybedtools.BedTool(args.sv_calls)
     for sv_call in sv_calls:
-        get_depth_for_sv_call(sv_call, bams_by_name, chromosome_sizes)
-
-    #Parallel(n_jobs=args.nproc, verbose=verbosity)(delayed(get_depth_for_sv_call)(sv_call, bams_by_name, chromosome_sizes) for sv_call in sv_calls)
+        get_depth_for_sv_call(sv_call, bams_by_name, chromosome_sizes, args.min_mapping_quality, args.min_base_quality, args.slop_for_insertion_breakpoints)
--- scripts/get_best_alignment.py
+++ scripts/get_best_alignment.py
@@ -1,7 +1,9 @@
 import argparse
 from collections import defaultdict
 import logging
+import numpy as np
 import pysam
+import sys
 
 # create logger
 logger = logging.getLogger(__name__)
@@ -23,6 +25,26 @@ logger.addHandler(ch)
 BAM_CMATCH = 0
 
 
+def get_depth_by_reference_and_position(alignments, bam, base_quality):
+    depth_by_reference_and_position = {}
+
+    for alignment in alignments:
+        ref = bam.getrname(alignment.reference_id)
+        if ref not in depth_by_reference_and_position:
+            depth_by_reference_and_position[ref] = defaultdict(int)
+
+        pairs = np.array([pair for pair in alignment.aligned_pairs if pair[0] is not None])
+
+        if len(pairs) != len(alignment.query_qualities):
+            raise Exception("Mismatch")
+
+        for base in pairs[np.array(alignment.query_qualities) >= base_quality][:,1]:
+            if base is not None:
+                depth_by_reference_and_position[ref][base] += 1
+
+    return depth_by_reference_and_position
+
+
 def calculate_differences_in_alignment(alignment):
     # Calculate indel and softclipped bases from the sum of lengths for CIGAR
     # options that aren't matches.
@@ -37,7 +59,7 @@ def calculate_differences_in_alignment(alignment):
     return differences
 
 
-def get_best_alignment(alignments_by_read_name, filtered_reads):
+def get_best_alignment(alignments_by_read_name):
     """
     For a given dictionary of lists of alignments indexed by read name and
     number (e.g., "read_name/1"), find the alignment with the fewest differences
@@ -56,6 +78,7 @@ def get_best_alignment(alignments_by_read_name, filtered_reads):
     HJ2HJCCXX160108:5:2220:13311:66496      83      40
     HJ2HJCCXX160108:5:2220:13311:66496      163     60
     """
+    filtered_alignments = []
     for name, alignments in alignments_by_read_name.iteritems():
         if len(alignments) > 1:
             # Calculate differences between read and the reference for each
@@ -72,23 +95,81 @@ def get_best_alignment(alignments_by_read_name, filtered_reads):
             # don't emit any alignments to be consistent with a mapping quality
             # of 0. If there is only one best alignment, emit that.
             if len(alignments_by_differences[min_differences]) == 1:
-                filtered_reads.write(alignments_by_differences[min_differences][0])
+                filtered_alignments.append(alignments_by_differences[min_differences][0])
                 logger.debug("Found best alignment for %s", name)
             else:
                 logger.debug("Skipping equally scored alignments for %s", name)
         else:
             # Emit the only read in the set.
-            filtered_reads.write(alignments[0])
+            filtered_alignments.append(alignments[0])
+
+    return filtered_alignments
+
+
+def get_best_alignments(bam, regions, quality):
+    # Load alignments for requested regions into memory and sort by read name.
+    # Do our own fancy splitting of regions by colon and then by hyphen to allow
+    # unpleasant local assembly contig names to be parsed correctly.
+    alignments_from_regions = [read
+                               for region in regions
+                               for read in bam.fetch(*([region.split(":")[0]] + map(int, region.split(":")[1].split("-"))))
+                               if read.mapping_quality >= quality]
+    logger.debug("Loaded %i reads from BAM from %i regions", len(alignments_from_regions), len(regions))
+
+    # Sort alignments by read name.
+    logger.debug("Sorting alignments by read name")
+    alignments_from_regions = sorted(alignments_from_regions, key=lambda alignment: alignment.query_name)
+
+    current_read_name = None
+    alignments_by_read_name = defaultdict(list)
+
+    logger.debug("Filtering alignments")
+    alignments_processed = 0
+    filtered_alignments_from_regions = []
+    for alignment in alignments_from_regions:
+        alignments_processed += 1
+        if alignment.query_name != current_read_name:
+            if current_read_name is not None:
+                filtered_alignments_from_regions.extend(get_best_alignment(alignments_by_read_name))
+
+            current_read_name = alignment.query_name
+            alignments_by_read_name = defaultdict(list)
+
+        # Index alignments by query name and read number to avoid treating readsn 1
+        # and 2 of the same pair as duplicates.
+        read_name = "%s/%s" % (alignment.query_name, alignment.is_read1 and 1 or 2)
+        alignments_by_read_name[read_name].append(alignment)
+    else:
+        # Get best alignments from the last set in the for loop.
+        filtered_alignments_from_regions.extend(get_best_alignment(alignments_by_read_name))
+
+    # Sort alignments by chromosome and start position.
+    logger.debug("Sorting alignments by chromosome and start position")
+    filtered_alignments_from_regions = sorted(
+        filtered_alignments_from_regions,
+        key=lambda alignment: (bam.getrname(alignment.reference_id), alignment.reference_start)
+    )
+
+    logger.debug("Processed %i alignments and retained %i", alignments_processed, len(filtered_alignments_from_regions))
+
+    # Return filtered alignments .
+    return filtered_alignments_from_regions
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("reads")
+    parser.add_argument("alignments")
+    parser.add_argument("output")
+    parser.add_argument("regions", nargs="+", help="one or more samtools-style regions (e.g., chr1:1000-2000) to filter")
+    parser.add_argument("--quality", type=int, default=20, help="minimum mapping quality for reads to output")
+    parser.add_argument("--sam", action="store_true", help="output SAM for debugging")
     parser.add_argument("--uncompressed", action="store_true", help="output uncompressed BAM for piping")
     parser.add_argument("--debug", action="store_true")
     args = parser.parse_args()
 
-    if args.uncompressed:
+    if args.sam:
+        write_mode = "w"
+    elif args.uncompressed:
         write_mode = "wbu"
     else:
         write_mode = "wb"
@@ -97,28 +178,9 @@ if __name__ == "__main__":
         logger.setLevel(logging.DEBUG)
         ch.setLevel(logging.DEBUG)
 
-    reads = pysam.AlignmentFile(args.reads, "rb")
-    filtered_reads = pysam.AlignmentFile("-", write_mode, template=reads)
+    with pysam.AlignmentFile(args.alignments, "rb") as bam:
+        best_alignments = get_best_alignments(bam, args.regions, args.quality)
 
-    current_read_name = None
-    alignments_by_read_name = defaultdict(list)
-
-    reads_processed = 0
-    for read in reads:
-        reads_processed += 1
-        if read.query_name != current_read_name:
-            if current_read_name is not None:
-                get_best_alignment(alignments_by_read_name, filtered_reads)
-
-            current_read_name = read.query_name
-            alignments_by_read_name = defaultdict(list)
-
-        # Index reads by query name and read number to avoid treating reads 1
-        # and 2 of the same pair as duplicates.
-        read_name = "%s/%s" % (read.query_name, read.is_read1 and 1 or 2)
-        alignments_by_read_name[read_name].append(read)
-    else:
-        # Get best reads from the last set in the for loop.
-        get_best_alignment(alignments_by_read_name, filtered_reads)
-
-    logger.debug("Processed %i reads for filtering", reads_processed)
+        with pysam.AlignmentFile(args.output, write_mode, template=alignments) as filtered_alignments:
+            for alignment in best_alignments:
+                filtered_alignments.write(alignment)
