23adc364b0096899e7631ed2961bbc5f2e12fadb
Peter Audano <paudano@gmail.com>
2016-08-02 16:37:25 -0700

    Fixed order of calls in smrtsv.py run target.

--- bin/smrtsv.py
+++ bin/smrtsv.py
@@ -314,18 +314,18 @@ def run(args):
         print("\t* Assemble: %s" % job_step[2])
         print("\t*     Call: %s" % job_step[3])
 
+    # Build reference indices
+    return_code = index(args)
+    if return_code != 0:
+        sys.stderr.write("Failed to index reference\n")
+        return return_code
+    
     # Align
     args.jobs = job_step[0]
 
     return_code = align(args)
     if return_code != 0:
         sys.stderr.write("Failed to align reads\n")
-        return return_code
-
-    # Build reference indices
-    return_code = index(args)
-    if return_code != 0:
-        sys.stderr.write("Failed to index reference\n")
         return return_code
 
     # Detect SVs.
