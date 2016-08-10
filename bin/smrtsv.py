839b10cd86e449cac8e58f8095c08454c3c494b0
Peter Audano <paudano@gmail.com>
2016-08-10 09:21:53 -0700

    Removed duplicate "alignment_parameters" option in run target

--- bin/smrtsv.py
+++ bin/smrtsv.py
@@ -490,7 +490,6 @@ if __name__ == "__main__":
     parser_runner.add_argument("--min_hardstop_support", type=int, help="minimum number of reads with hardstops required to flag a region as an SV candidate", default=11)
     parser_runner.add_argument("--max_candidate_length", type=int, help="maximum length allowed for an SV candidate region", default=60000)
     parser_runner.add_argument("--rebuild_regions", action="store_true", help="rebuild subset of regions to assemble")
-    parser_runner.add_argument("--alignment_parameters", help="BLASR parameters to use to align local assemblies", default="-affineAlign -affineOpen 8 -affineExtend 0 -bestn 1 -maxMatch 30 -sdpTupleSize 13")
     parser_runner.add_argument("--mapping_quality", type=int, help="minimum mapping quality of raw reads to use for local assembly", default=30)
     parser_runner.add_argument("--minutes_to_delay_jobs", type=int, help="maximum number of minutes to delay local assembly jobs to limit simultaneous I/O on shared storage", default=1)
     parser_runner.add_argument("--assembly_log", help="name of log file for local assemblies", default="assembly.log")
