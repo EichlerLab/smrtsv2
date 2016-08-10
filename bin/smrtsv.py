61e1651b12800b8d58227436043b2c1d4100dd9c
Peter Audano <paudano@gmail.com>
2016-08-10 09:13:19 -0700

    Added new options from other targets to the run target in smrtsv.py.

--- bin/smrtsv.py
+++ bin/smrtsv.py
@@ -468,7 +468,7 @@ if __name__ == "__main__":
     parser_caller.add_argument("--species", help="Common or scientific species name to pass to RepeatMasker", default="human")
     parser_caller.set_defaults(func=call)
 
-    # Call SVs and indels from BLASR alignments of raw reads.
+    # Run: Call SVs and indels from BLASR alignments of raw reads.
     parser_runner = subparsers.add_parser("run", help="call SVs and indels by local assembly of BLASR-aligned reads")
     parser_runner.add_argument("reference", help="FASTA file of indexed reference with .ctab and .sa in the same directory")
     parser_runner.add_argument("reads", help="text file with one absolute path to a PacBio reads file (.bax.h5) per line")
@@ -482,7 +482,18 @@ if __name__ == "__main__":
     parser_runner.add_argument("--exclude", help="BED file of regions to exclude from local assembly (e.g., heterochromatic sequences, etc.)")
     parser_runner.add_argument("--assembly_window_size", type=int, help="size of reference window for local assemblies", default=60000)
     parser_runner.add_argument("--assembly_window_slide", type=int, help="size of reference window slide for local assemblies", default=30000)
+    parser_runner.add_argument("--min_length", type=int, help="minimum length required for SV candidates", default=50)
+    parser_runner.add_argument("--min_support", type=int, help="minimum number of supporting reads required to flag a region as an SV candidate", default=5)
+    parser_runner.add_argument("--max_support", type=int, help="maximum number of supporting reads allowed to flag a region as an SV candidate", default=100)
+    parser_runner.add_argument("--min_coverage", type=int, help="minimum number of total reads required to flag a region as an SV candidate", default=5)
+    parser_runner.add_argument("--max_coverage", type=int, help="maximum number of total reads allowed to flag a region as an SV candidate", default=100),
+    parser_runner.add_argument("--min_hardstop_support", type=int, help="minimum number of reads with hardstops required to flag a region as an SV candidate", default=11)
+    parser_runner.add_argument("--max_candidate_length", type=int, help="maximum length allowed for an SV candidate region", default=60000)
     parser_runner.add_argument("--rebuild_regions", action="store_true", help="rebuild subset of regions to assemble")
+    parser_runner.add_argument("--alignment_parameters", help="BLASR parameters to use to align local assemblies", default="-affineAlign -affineOpen 8 -affineExtend 0 -bestn 1 -maxMatch 30 -sdpTupleSize 13")
+    parser_runner.add_argument("--mapping_quality", type=int, help="minimum mapping quality of raw reads to use for local assembly", default=30)
+    parser_runner.add_argument("--minutes_to_delay_jobs", type=int, help="maximum number of minutes to delay local assembly jobs to limit simultaneous I/O on shared storage", default=1)
+    parser_runner.add_argument("--assembly_log", help="name of log file for local assemblies", default="assembly.log")
     parser_runner.add_argument("--refindex", action="store_true", help="Generate a BLASR index on the reference sequence.")
     parser_runner.add_argument("--sample", help="Sample name to use in final variant calls", default="UnnamedSample")
     parser_runner.add_argument("--species", help="Common or scientific species name to pass to RepeatMasker", default="human")
