64ba0c865807eba09f5f0405330312ef5ebe3632
Peter Audano <paudano@gmail.com>
2017-06-30 07:57:02 -0700

    SMRTSV GT checks contig set integrity.
    * Input contigs must not be repeated
    * All contigs over all SVs must be present

--- scripts/genotype/FilterContigs.py
+++ scripts/genotype/FilterContigs.py
@@ -14,13 +14,35 @@ def filter_bam(input_file, queries, output_file, output_sizes_file):
     :param output_sizes_file: File query sizes are written to.
     """
 
+    found_queries = set()
+
     with pysam.AlignmentFile(input_file, 'rb') as input_bam:
         with pysam.AlignmentFile(output_file, 'w', template=input_bam) as output_bam:
             with open(output_sizes_file, 'w') as output_sizes:
                 for alignment in input_bam:
                     if alignment.query_name in queries:
+
+                        # Check for multiple records with the same query
+                        if alignment.query_name in found_queries:
+                            raise ValueError('Query has multiple records in the input BAM: {}'.format(alignment.query_name))
+
+                        found_queries.add(alignment.query_name)
+
                         output_bam.write(alignment)
                         output_sizes.write('{}\t{}\n'.format(alignment.query_name, alignment.query_length))
+
+    # Check for missing queries
+    missing_queries = queries - found_queries
+    n_missing = len(missing_queries)
+
+    if n_missing > 0:
+
+        missing_str = ', '.join(sorted(list(missing_queries))[:3])
+
+        if n_missing > 3:
+            missing_str += '...'
+
+        raise ValueError('Missing {} query sequences while filtering BAM: {}'.format(n_missing, missing_str))
 
 
 def filter_sam(input_file, queries, output_file, output_sizes_file):
@@ -32,6 +54,8 @@ def filter_sam(input_file, queries, output_file, output_sizes_file):
     :param output_file: Output SAM file.
     :param output_sizes_file: File query sizes are written to.
     """
+
+    found_queries = set()
 
     with open(input_file, 'r') as input_sam:
         with open(output_file, 'w') as output_sam:
@@ -48,8 +72,28 @@ def filter_sam(input_file, queries, output_file, output_sizes_file):
                     tok = line.split('\t')
 
                     if tok[0] in queries:
+
+                        # Check for multiple records with the same query
+                        if tok[0] in found_queries:
+                            raise ValueError('Query has multiple records in the input SAM: {}'.format(tok[0]))
+
+                        found_queries.add(tok[0])
+
                         output_sam.write(line)
                         output_sizes.write('{}\t{}\n'.format(tok[0], len(tok[9])))
+
+    # Check for missing queries
+    missing_queries = queries - found_queries
+    n_missing = len(missing_queries)
+
+    if n_missing > 0:
+
+        missing_str = ', '.join(sorted(list(missing_queries))[:3])
+
+        if n_missing > 3:
+            missing_str += '...'
+
+        raise ValueError('Missing {} query sequences while filtering SAM: {}'.format(n_missing, missing_str))
 
 if __name__ == '__main__':
 
